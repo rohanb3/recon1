c1d51248fabbf5758758b41a6cf4881f478ae772
Igor Retiznyk <igor.retiznyk@ardas.biz>
2019-05-23 18:04:56 +0300

    Update boundaries.js

--- src/filters/boundaries.js
+++ src/filters/boundaries.js
@@ -15,7 +15,7 @@ export default (value, { prefix = '' } = {}) => {
   const [first, last] = Array.isArray(value) ? getPointsForArray(value) : getPointsForObject(value);
   const from = notEmpty(first) ? `${i18n.t('from')} ${first}` : '';
   const to = notEmpty(last) ? `${i18n.t('to')} ${last}` : '';
-  const result = [from, to].filter(i => i).join(' ');
+  const result = [from, to].filter(Boolean).join(' ');
 
   return result ? `${prefix}${result}` : '';
 };
