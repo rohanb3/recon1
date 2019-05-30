65fc23b1ce5ea4cb962090c340f80b6d144ec9d8
Vlad <vladislavmoiseenkov@gmail.com>
2019-05-30 12:01:47 +0300

    Add notification if unexisting user attempts to restore password

--- src/containers/PasswordRecoveryPage.vue
+++ src/containers/PasswordRecoveryPage.vue
@@ -9,6 +9,7 @@ import PasswordRecoveryForm from '@/components/PasswordRecoveryForm';
 import { requestVerificationCode } from '@/services/identityRepository';
 import { SET_EMAIL } from '@/store/loggedInUser/mutationTypes';
 import { ROUTE_NAMES } from '@/constants';
+import { errorMessage } from '@/services/notifications';
 
 export default {
   name: 'PasswordRecoveryPage',
@@ -17,10 +18,22 @@ export default {
   },
   methods: {
     async sendVerificationCode(email) {
-      const emailLowerCase = email.toLowerCase();
-      await requestVerificationCode(emailLowerCase);
-      this.$store.commit(SET_EMAIL, emailLowerCase);
-      this.$router.push({ name: ROUTE_NAMES.VERIFICATION_CODE });
+      try {
+        const emailLowerCase = email.toLowerCase();
+        await requestVerificationCode(emailLowerCase);
+        this.$store.commit(SET_EMAIL, emailLowerCase);
+        this.$router.push({ name: ROUTE_NAMES.VERIFICATION_CODE });
+      } catch (e) {
+        const {
+          response: {
+            data: { errors: message },
+          },
+        } = e;
+
+        if (message.Email) {
+          errorMessage('email.is.not.registered');
+        }
+      }
     },
   },
 };
