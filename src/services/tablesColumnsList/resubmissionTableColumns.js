09bcadca17f17f68208e1014fd4b057c2e9def7a
Vlad <vladislavmoiseenkov@gmail.com>
2019-06-07 11:55:00 +0300

    Implement the sorting by Order Number on the Disputes list and Resubmission list

--- src/services/tablesColumnsList/resubmissionTableColumns.js
+++ src/services/tablesColumnsList/resubmissionTableColumns.js
@@ -21,6 +21,8 @@ export default () => [
     title: i18n.t('dispute.orders.number'),
     width: '4',
     minWidth: '16px',
+    sortingFieldName: TABLE_DISPUTE_COLUMNS_SORTED.ORDER_NUMBER,
+    fieldHeaderType: 'sortingHeader',
   },
   {
     name: 'fiscalPeriod',
