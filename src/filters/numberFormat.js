85784e06c62156695cb3a8575da395c1f5626606
Dmitry <d.drehval@gmail.com>
2019-06-20 14:17:46 +0300

    bages are now displayed when data is not received

--- src/filters/numberFormat.js
+++ src/filters/numberFormat.js
@@ -5,5 +5,5 @@ export const addSpaceBetweenNumbers = number => {
 };
 
 export const percents = number => {
-  return number || number === 0 ? `${number}%` : '';
+  return number || number === 0 ? `${number}%` : `${0}%`;
 };
