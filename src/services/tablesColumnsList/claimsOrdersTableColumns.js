d6f00c8fe8c88dd1319a7c0706e5cb4ec056c42b
Vlad <vladislavmoiseenkov@gmail.com>
2019-10-03 17:45:28 +0300

    some changes

--- src/services/tablesColumnsList/claimsOrdersTableColumns.js
+++ src/services/tablesColumnsList/claimsOrdersTableColumns.js
@@ -7,7 +7,7 @@ export default () => [
     field: 'orderNumber',
     fieldType: 'orderNumber',
     class: 'text',
-    title: i18n.t('company.order.number'),
+    title: i18n.t('orders.number'),
     width: '4',
     minWidth: '90px',
     sortingFieldName: TABLE_ORDER_COLUMNS_SORTED.NUMBER,
