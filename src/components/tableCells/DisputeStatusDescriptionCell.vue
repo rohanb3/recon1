877a69e02f227c848fcd8e9b53d3ed8fc0d3e457
Dobrovolskyi Yuriy <yariydobrovolskyi@gmail.com>
2019-10-07 17:45:05 +0300

    added popup for description cell

--- src/components/tableCells/DisputeStatusDescriptionCell.vue
+++ src/components/tableCells/DisputeStatusDescriptionCell.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="dispute-status-description-cell" :title="disputeComment">
-    {{ disputeComment | dashForEmptyValue }}
+    <v-menu
+      v-model="value"
+      offset-x
+      left
+      close-on-click
+      nudge-left="10"
+      max-width="200"
+      min-width="200"
+      transition="scale-transition"
+    >
+      <template v-slot:activator="{ on }">
+        <a @click.prevent v-on="on">{{ disputeComment | dashForEmptyValue }}</a>
+      </template>
+      <div @click.stop>
+        <v-list class="pa-3 word-wrap">
+          {{ item.statusDescription }}
+        </v-list>
+      </div>
+    </v-menu>
   </div>
 </template>
 
 <script>
 import dashForEmptyValue from '@/filters/dashForEmptyValue';
+import { addBackgroundShadow, removeBackgroundShadow } from '@/services/background';
 
 export default {
   name: 'DisputeStatusDescriptionCell',
@@ -22,11 +41,33 @@ export default {
       return this.item.submitterComment || this.item.statusDescription;
     },
   },
+  data: () => ({
+    value: false,
+  }),
+  watch: {
+    value(value) {
+      if (value) {
+        addBackgroundShadow();
+      } else {
+        removeBackgroundShadow();
+      }
+    },
+  },
+  beforeDestroy() {
+    if (this.value) {
+      removeBackgroundShadow();
+      this.value = false;
+    }
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .dispute-status-description-cell {
   font-style: italic;
+}
+
+.word-wrap {
+  word-wrap: break-word;
 }
 </style>
